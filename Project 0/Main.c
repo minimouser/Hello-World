22ec827aac639658595e56b6ab9079f0a2a1b63a
minimouser <minimouser@gmail.com>
2019-01-14 15:50:36 -0800

    Initial commit

---
+++ Project 0/Main.c
@@ -0,0 +1,14 @@
+/*
+	Name: Bailey Wolber
+	Date: 1/11/2019
+	Email: Bailey.Wolber@wsu.edu
+	Description: prints out a single line of text that says "Hello World"
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+	return 0; // indicates success
+}
